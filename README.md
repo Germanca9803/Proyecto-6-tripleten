e69ce2bc7e717e692a4864ded54044c6339c4c0a
Germanca9803 <germanuber18@gmail.com>
2024-08-22 15:31:16 -0700

    Agrego ruta de data correcta

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Proyecto-6-tripleten
 Las tareas incluyen la creación y gestión de entornos virtuales de Python, el desarrollo de una aplicación web y su despliegue en un servicio en la nube que la hará accesible al público.
+
+URL: https://proyecto-6-tripleten.onrender.com
--- app.py
+++ app.py
@@ -2,17 +2,49 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-car_data = pd.read_csv(
-    'C:/Users/japay/Documents/TRIPETEN_CURSO/Proyecto6/Proyecto-6-tripleten/vehicles_us.csv')  # leer los datos
-hist_button = st.button('Construir histograma')  # crear un botón
+car_data = pd.read_csv('./vehicles_us.csv')  # leer los datos
 
-if hist_button:  # al hacer clic en el botón
+
+build_bar_graph = st.checkbox('Construir un grafico de barras')
+build_histogram = st.checkbox('Construir un histograma')
+build_scatter = st.checkbox('Construir un grafico de dispersion')
+
+if build_histogram:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x=(
+        ['odometer', 'price']), title='Histogramas')
+    fig.show()
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+if build_bar_graph:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un grafico de barras para el conjunto de datos por tipo de transmicion')
+
+    # crear un grafico de barras
+    df = car_data.groupby('transmission').count()
+    fig3 = px.bar(df, y=(['price', 'fuel']))
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig3, use_container_width=True)
+
+if build_scatter:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creacion de un grafico de dispersion'
+    )
+    # crear un grafico de barras
+    fig2 = px.scatter(car_data, x="model_year", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
+
+st.write(
+    # Echo por German Camacho Angulo
+)
